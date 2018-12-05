42b500af7362f994cd16b7447f86c97f7fd45fb6
sirpask <el_pask@hotmail.com>
2018-12-05 17:47:02 +0100

    Add files via upload

---
+++ rascar_web.py
@@ -0,0 +1,24 @@
+# import libraries
+import urllib.request
+from bs4 import BeautifulSoup
+
+# specify the url
+quote_page = 'http://www.meteovillaverde.es'
+
+# query the website and return the html to the variablb ‘page’
+page = urllib.request.urlopen(quote_page)
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(page, 'html.parser')
+
+var1 = 0
+for sub_heading in soup.find_all('td'):
+    if sub_heading.text == ' Temperatura:':
+        var1 = 1
+    else:
+        if var1 == 1:
+            Temperatura = sub_heading.text
+            break
+
+
+print(Temperatura)
